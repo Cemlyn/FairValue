f35bc951dbb03ca9ee7e95706c3396a03be84566
Cemlyn Coirier-Roberts <cemlyn.coro@gmail.com>
2025-02-21 20:16:26 +0000

    Delete FairValue directory
    duplicated

--- FairValue/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._stock import (
-    Stock,
-)
-
-from ._exceptions import ParseException
-from ._ingestion import secfiling_to_financials
--- FairValue/_exceptions.py
+++
@@ -1,9 +0,0 @@
-from pydantic import ValidationError
-
-
-class ParseException(Exception):
-    pass
-
-
-class FairValueException(Exception):
-    pass
